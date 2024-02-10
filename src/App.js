a2f91a4acd1564c1c20eb4e15c63341dc0ec0021
nweligalla <nayanaweligalla@gmail.com>
2024-02-10 16:58:38 +0530

    added social icons

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 
 
@@ -5,6 +6,7 @@ function App() {
   return (
     <h1>
       <Navbar/>
+      <Home/>
     </h1>
   );
 }
