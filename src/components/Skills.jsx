25c5348d71d56a1cb9f77d28b1936b3b63080793
nweligalla <nayanaweligalla@gmail.com>
2024-02-11 10:02:16 +0530

    added base theme

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -12,48 +12,48 @@ import Mongo from '../assets/mongo.png';
 
 function Skills() {
     return (
-        <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+        <div name='skills' className='w-full h-screen'>
             <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
                 <div>
-                    <p className='text-4xl inline border-b-4 border-pink-600  '>Skills</p>
+                    <p className='section-title'>Skills</p>
                     <p className='py-4'>// Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus, accusamus.</p>
                 </div>
                 <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
-                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                        <img className='w-20 mx-auto' src={HTML} alt="HTML icon" />
-                        <p className='my-4'>HTML</p>
+                    <div className='skill-card'>
+                        <img src={HTML} alt="HTML icon" />
+                        <p >HTML</p>
                     </div>
-                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                        <img className='w-20 mx-auto' src={CSS} alt="HTML icon" />
-                        <p className='my-4'>CSS</p>
+                    <div className='skill-card'>
+                        <img src={CSS} alt="HTML icon" />
+                        <p >CSS</p>
                     </div>
-                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                        <img className='w-20 mx-auto' src={JavaScript} alt="HTML icon" />
-                        <p className='my-4'>JAVASCRIPT</p>
+                    <div className='skill-card'>
+                        <img src={JavaScript} alt="HTML icon" />
+                        <p >JAVASCRIPT</p>
                     </div>
-                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                        <img className='w-20 mx-auto' src={ReactImg} alt="HTML icon" />
-                        <p className='my-4'>REACT</p>
+                    <div className='skill-card'>
+                        <img src={ReactImg} alt="HTML icon" />
+                        <p >REACT</p>
                     </div>
-                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                        <img className='w-20 mx-auto' src={GitHub} alt="HTML icon" />
-                        <p className='my-4'>GITHUB</p>
+                    <div className='skill-card'>
+                        <img src={GitHub} alt="HTML icon" />
+                        <p >GITHUB</p>
                     </div>
-                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                        <img className='w-20 mx-auto' src={Node} alt="HTML icon" />
-                        <p className='my-4'>NODE JS</p>
+                    <div className='skill-card'>
+                        <img src={Node} alt="HTML icon" />
+                        <p >NODE JS</p>
                     </div>
-                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                        <img className='w-20 mx-auto' src={Mongo} alt="HTML icon" />
-                        <p className='my-4'>MONGO DB</p>
+                    <div className='skill-card'>
+                        <img src={Mongo} alt="HTML icon" />
+                        <p >MONGO DB</p>
                     </div>
-                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                        <img className='w-20 mx-auto' src={Tailwind} alt="HTML icon" />
-                        <p className='my-4'>TAILWIND</p>
+                    <div className='skill-card'>
+                        <img src={Tailwind} alt="HTML icon" />
+                        <p >TAILWIND</p>
                     </div>
-                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                        <img className='w-20 mx-auto' src={FireBase} alt="HTML icon" />
-                        <p className='my-4'>FIREBASE</p>
+                    <div className='skill-card'>
+                        <img src={FireBase} alt="HTML icon" />
+                        <p>FIREBASE</p>
                     </div>
                 </div>
             </div>
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -6,10 +6,10 @@ import RealEst from '../assets/projects/realestate.jpg'
 
 function Work() {
     return (
-        <div name='work' className='w-full md:h-screen bg-[#0a192f] text-gray-300'>
+        <div name='work' className='w-full md:h-screen '>
             <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
                 <div className='pb-8'>
-                    <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>Work</p>
+                    <p className='section-title'>Work</p>
                     <p className='py-6'>// Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsam?</p>
                 </div>
                 {/* container */}
