8318ae683a7b8d6c2bf506e1860d9d0c7ba6e175
nweligalla <nayanaweligalla@gmail.com>
2024-02-11 14:05:11 +0530

    Fixed responsive issue of projects

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -12,7 +12,7 @@ import Mongo from '../assets/mongo.png';
 
 function Skills() {
     return (
-        <div name='skills' className='w-full h-screen'>
+        <div name='skills' className='w-full min-h-screen'>
             <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
                 <div>
                     <p className='section-title'>Skills</p>
--- src/components/resusables/ProjectCard.jsx
+++ src/components/resusables/ProjectCard.jsx
@@ -2,21 +2,19 @@ import React from 'react'
 
 function ProjectCard({ projectName, description, time, techStack, company }) {
     return (
-        <li className="flex items-start space-x-3 bg-[#31304D] p-4">
-            <span rel="noopener noreferrer" href="#" className="flex items-center h-8 text-sm hover:underline">{time}</span>
+        <li className="min-w-[100%] cu flex cursor-default items-start space-x-3 bg-[#31304D] p-4">
+            <span className="flex items-center mt-1 h-8 text-sm hover:underline">{time}</span>
             <div className="flex-1 space-y-2">
                 <div className="flex items-center justify-between space-x-4 dark:text-gray-400">
-                    <span rel="noopener noreferrer" href="#" className="inline-flex items-center px-3 pb-1 my-1 space-x-2 text-sm group dark:border-gray-700">
-                        <span aria-hidden="true" className="h-1.5 w-1.5 rounded-full dark:bg-violet-400"></span>
-                        <span className=" text-lg dark:text-gray-100">{projectName}</span>
-
+                    <span className="inline-flex items-center pb-1 my-1 space-x-2 text-sm group dark:border-gray-700">
+                        <span className=" text-lg font-bold dark:text-gray-100">{projectName}</span>
                     </span>
                     <span className=" text-sm">{company}</span>
                 </div>
                 <div>
                     <p>{description}</p>
                 </div>
-                <div className='mt-1'>
+                <div className='pt-2 flex flex-row flex-wrap'>
                     {techStack.map(function (tech) {
                         return (
                             <span className="bg-gray-100 text-gray-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">
